aad2b6a25a47bbb662f8d540338c7b9ac1a2ca7b
Doniyor Ro'ziqulov <doniyorruziqulov33@gmail.com>
2025-06-02 19:19:04 +0500

    Added

--- src/components/jurnals/jurnals.jsx
+++ src/components/jurnals/jurnals.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaDownload } from 'react-icons/fa6'
 import book1 from '../images/book1.png'
 import book2 from '../images/book2.jpg'
 import book3 from '../images/book3.png'
@@ -9,6 +10,18 @@ const jurnals = () => {
   return (
     <div className="mt-[150px] mb-[100px]">
       <div className="container ">
+        <div className="flex  items-center">
+          <a
+            className="m-10 flex  items-center gap-x-1"
+            href="/International.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            download
+          >
+            <FaDownload className="text-3xl border p-0.5" />
+            <p className="text-2xl">Academs Space PDF</p>
+          </a>
+        </div>
         <ul className="flex justify-between gap-x-3 items-center">
           <li className="border border-black">
             <img className="w-[130px] lg:w-[280px]" src={book1} alt="" />
